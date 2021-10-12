bb9ba1416df8655d8a4cd7b4b6e93a036b63236a
Marco Taubert <marcotaub@gmail.com>
2021-10-12 15:17:50 +0200

    Add Quasar to app

---
+++ app/src/quasar-user-options.js
@@ -0,0 +1,14 @@
+import './styles/quasar.scss';
+import iconSet from 'quasar/icon-set/mdi-v4.js';
+import lang from 'quasar/lang/de.js';
+import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/mdi-v4/mdi-v4.css';
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  lang,
+  iconSet,
+};
